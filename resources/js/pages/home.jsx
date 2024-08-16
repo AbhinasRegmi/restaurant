9d22a6d0fb5933686bc7021de5bf260e6937354b
abhinas regmi <aviregmi2018@gmail.com>
2024-08-16 22:13:57 +0545

    chore: main: setup inertia and shadcn

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,11 @@
+import { Head } from "@inertiajs/react";
+
+export default function Home()
+{
+    return (
+        <main>
+            <Head title="Easy Restraunt"/>
+            Hello world.
+        </main>
+    )
+}
