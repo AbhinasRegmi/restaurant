daf9f71b7e7c81a454c9ee5b813227137ca8c32d
abhinas regmi <aviregmi2018@gmail.com>
2024-09-09 14:09:23 +0545

    fix: add background image in offer

--- resources/js/components/offer.jsx
+++ resources/js/components/offer.jsx
@@ -5,7 +5,7 @@ export function Offer() {
     const offerValidDate = new Date("2024-10-12");
 
     return (
-        <section className="flex flex-col lg:flex-row gap-y-8 lg:gap-y-0 bg-black py-16 lg:py-32 text-primary-foreground">
+        <section className="flex flex-col lg:flex-row gap-y-8 lg:gap-y-0 bg-black py-16 lg:py-32 text-primary-foreground lg:bg-[url('/offerBg.png')] ">
             <div className="flex-1 flex items-center justify-center">
                 <div className="text-center flex flex-col items-center gap-y-5 px-8 text-pretty font-medium">
                     <h1 className="text-3xl font-semibold">
