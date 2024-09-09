4183bcfa1695135e2c9388f8beb2ae60841e252a
abhinas regmi <aviregmi2018@gmail.com>
2024-09-09 14:01:56 +0545

    feat: Add Offer Component

--- resources/js/components/offer.jsx
+++ resources/js/components/offer.jsx
@@ -1,8 +1,34 @@
-export function Offer()
-{
+import { Button } from "@/components/ui/button";
+import Countdown from "react-countdown";
+
+export function Offer() {
+    const offerValidDate = new Date("2024-10-12");
+
     return (
-        <div>
-            This is an offer.
-        </div>
-    )
+        <section className="flex flex-col lg:flex-row gap-y-8 lg:gap-y-0 bg-black py-16 lg:py-32 text-primary-foreground">
+            <div className="flex-1 flex items-center justify-center">
+                <div className="text-center flex flex-col items-center gap-y-5 px-8 text-pretty font-medium">
+                    <h1 className="text-3xl font-semibold">
+                        Delicious Burger & French Fries
+                    </h1>
+                    <p>
+                        Progressively simplify effective e-toilers and process
+                        centric methods of employment. Quickly pontificate
+                        paralle.
+                    </p>
+                    <div className="text-4xl text-yellow-400">
+                        <Countdown date={offerValidDate} />
+                    </div>
+                    <Button>Order Now</Button>
+                </div>
+            </div>
+            <div className="flex-1 shrink-0 flex items-center justify-center">
+                <img
+                    className="h-full w-full object-cover rounded-sm"
+                    src="/offerProduct.png"
+                    alt="Offer item"
+                />
+            </div>
+        </section>
+    );
 }
